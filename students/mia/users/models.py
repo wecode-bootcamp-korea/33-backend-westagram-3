35b6afe6797e64d9222da3de720add67421668a2
Mia Lee <mia@Miaui-MacBookPro.local>
2022-05-13 13:08:27 +0900

    User App - models.py edit 3

--- students/mia/users/models.py
+++ students/mia/users/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 class User(models.Model): 
     name          = models.CharField(max_length=50)
-    email         = models.EmailField(max_length =100, unique=True)
+    email         = models.CharField(max_length =100, unique=True)
     password      = models.CharField(max_length=100)
     mobile_number = models.CharField(max_length=50)
     created_at    = models.DateTimeField(auto_now_add=True)
-    updated_at    = models.DateTimeField(auto_now_add=True)
+    updated_at    = models.DateTimeField(auto_now=True)
     
     class Meta: 
         db_table = "users"
