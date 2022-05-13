78765d5678d7c7ce8a0674a5804757c679626a19
박준형 <>
2022-05-13 21:04:34 +0900

    edit

--- students/junhyung/users/models.py
+++ students/junhyung/users/models.py
@@ -5,7 +5,6 @@ class User(models.Model):
     user_name = models.CharField(max_length = 20) 
     user_mail = models.CharField(max_length = 20)
     user_password = models.CharField(max_length = 20)
-    user_password = models.CharField(max_length = 20)
     user_number = models.CharField(max_length = 15)
 
     created_at = models.DateTimeField(auto_now_add = True)
