b38163551d1aad972bd940f442815986e6e2e841
박준형 <>
2022-05-14 17:48:51 +0900

    edit project modelling

--- students/junhyung/users/models.py
+++ students/junhyung/users/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_name = models.CharField(max_length = 20) 
-    user_mail = models.CharField(max_length = 20)
-    user_password = models.CharField(max_length = 20)
-    user_number = models.CharField(max_length = 15)
+    name = models.CharField(max_length = 20) 
+    mail = models.CharField(max_length = 50, unique = True)
+    password = models.CharField(max_length = 100)
+    number = models.CharField(max_length = 15)
 
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
