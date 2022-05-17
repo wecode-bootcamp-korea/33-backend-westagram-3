6a090607d7aeea656c772b8a0b750f1f998e3bfb
Mia Lee <mia@Miaui-MacBookPro.local>
2022-05-17 16:00:24 +0900

    User App - Password bcrypt5

--- students/mia/users/views.py
+++ students/mia/users/views.py
@@ -11,18 +11,17 @@ from .models import User
 
 class SignUpView(View):
     def post(self, request): 
-        try                : 
+        try: 
             input_data = json.loads(request.body)
             email      = input_data['email']
             password   = input_data['password']
 
-            # 기존에 존재하는 이메일이 중복되면 중복 에러 메세지 반환
             if User.objects.filter(email = email).exists():
                 return JsonResponse({"message" : "THE_USER_EMAIL_ALREADY_EXISTS"}, status=400)
-            # 이메일에 @ 또는 .이 없으면 에러 반환 
+
             if not re.match(r"^[a-zA-Z0-9+-_.]+@/[a-zA-Z0-9-.]+\.[a-zA-Z0-9-]$", email):
                 return JsonResponse({"message" : "INVALID_EMAIL_--_NEEDS_@_AND_."}, status=400)
-            # 비밀번호는 8자리 이상, 문자 숫자, 특수문자의 복합 -- 그러지 않을 경우, 에러 반환
+         
             if not re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$", password):
                 return JsonResponse({"message" : "INVALID_PASSWORD"}, status=400)
 
@@ -44,13 +43,10 @@ class SignInView(View):
         try:
             input_data = json.loads(request.body)
 
-            # non-existent log-in info & wrong password
             if not User.objects.filter(email = input_data['email'], password = input_data['password']).exists():
                 return JsonResponse({"message" : "INVALID_USER"}, status=401)
 
-            # log-in success
             return JsonResponse({"message" : "SUCCESS"}, status=200)
 
-            # account/password not delievered
         except KeyError:
             return JsonResponse({"message" : "KEY_ERROR"}, status=400)
